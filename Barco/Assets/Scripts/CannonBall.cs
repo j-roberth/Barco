afbb8dd0bafd6a125e1c36cda0530a0e4658c52d
Nelson Scarinci <nelsonngs92@gmail.com>
2026-05-07 19:42:18 -0300

    Added cannon and cannon ball

---
+++ Barco/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Vector3 _ballDirection;
+
+    public void SetDirection(Vector3 direction)
+    { 
+        _ballDirection = direction;
+    }
+
+    private void Update()
+    {
+        //Vector3 ballMovement = new Vector3(_ballDirection.x * _speed * Time.deltaTime, _ballDirection.y * _speed * Time.deltaTime, _ballDirection.z * _speed * Time.deltaTime);
+
+        Vector3 ballMovement = _ballDirection * _speed * Time.deltaTime;
+
+        transform.position += ballMovement;
+    }
+
+    //Agregar deteccion contra el player (OnCollisionEnter)
+    //HAcerle daño player (hay que hacer un sistema de vida)
+    //Destruir ésta instancia de bola de cañon. Destroy(gameObject);
+}
