eb643616fd726b52764793b0530691d3f9bff088
Robert Guerrero <sertec@Warrior-MacBook-Air.local>
2026-05-14 18:09:10 -0500

    El cañón quedó automatizado

--- Barco/Assets/Scripts/Cannon.cs
+++ Barco/Assets/Scripts/Cannon.cs
@@ -6,15 +6,29 @@ public class Cannon : MonoBehaviour
     [SerializeField] private GameObject _cannonBallPrefab;
     [SerializeField] private Transform _cannonTip;
     [SerializeField] private GameObject _playerGO;
+    [SerializeField] private float _shootTime;
+    private float _shootTimer;
 
+    void Start()
+    {   
+        _shootTimer = _shootTime;
+    }   
+    
     private void Update()
-    {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame) //Cambiar la forma en la que se spawnean las balas. Hacer que sea automaticamente cuando el player está a cierto rango. Y que dispare con una cadencia (por ejemplo cada 5 segundos).
+    {   
+        _shootTimer -= Time.deltaTime;
+        if (_shootTimer <=0)          
         {
-            var directionToPlayer = _playerGO.transform.position - _cannonTip.position;
+            _shootTimer = _shootTime;
+          Shoot();
+        }
+    }
+    
+    private void Shoot ()
+    {
+        var directionToPlayer = _playerGO.transform.position - _cannonTip.position;
 
             var cannonBall = Instantiate(_cannonBallPrefab, _cannonTip.position, Quaternion.identity);
             cannonBall.GetComponent<CannonBall>().SetDirection(directionToPlayer);
-        }
     }
 }
