52a639bc540c07cdd5067d9ef37349e0215bb4fb
Robert Guerrero <sertec@Warrior-MacBook-Air.local>
2026-06-18 18:00:03 -0500

    Texto final
    añadí texto final al terminar el juego

--- Barco/Assets/Scripts/GoalZone.cs
+++ Barco/Assets/Scripts/GoalZone.cs
@@ -5,15 +5,12 @@ public class GoalZone : MonoBehaviour
 {
     [SerializeField] private Cannon[] _cannonsToDisable;
     [SerializeField] private GameObject _winText;
+    [SerializeField] private GameObject _finishPanel;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Entró algo: " + other.name);
-
         if (other.CompareTag("Player"))
         {
-            Debug.Log("¡Llegaste al muelle!");
-
             foreach (Cannon cannon in _cannonsToDisable)
             {
                 if (cannon != null)
@@ -22,12 +19,25 @@ public class GoalZone : MonoBehaviour
                 }
             }
 
-            if (_winText != null)
+            // Tutorial → cargar MainScene
+            if (SceneManager.GetActiveScene().name == "Level1_Tutorial")
             {
-                _winText.SetActive(true);
+                if (_winText != null)
+                {
+                    _winText.SetActive(true);
+                }
+
+                SceneManager.LoadScene("MainScene");
             }
 
-            SceneManager.LoadScene("MainScene");
+            // MainScene → terminar el juego
+            else if (SceneManager.GetActiveScene().name == "MainScene")
+            {
+                if (_finishPanel != null)
+                {
+                    _finishPanel.SetActive(true);
+                }
+            }
         }
     }
 }
